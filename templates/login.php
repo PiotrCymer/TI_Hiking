81469e42b261e20fbeb85d71a5ce5bdfc1581acb
PiotrCymer <piotr@cymer.pl>
2021-01-08 12:39:21 +0100

    fix: few changes in login

--- templates/login.php
+++ templates/login.php
@@ -1,7 +1,7 @@
 <form class="form-signin text-center" method="post" id="signInForm">
     <i class="fas fa-hiking" style="font-size: 80px;"></i>
-    <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-    <h5 class=" mb-3 font-weight-normal">To explore and manage your trips!</h5>
+    <h1 class="h3 mb-3 font-weight-normal">Zaloguj się</h1>
+    <h5 class=" mb-3 font-weight-normal">Aby dodawać i przeglądać swoje wędrówki!</h5>
     <div class="alert alert-danger" role="alert" id="loginError"></div>
     <input type="email" id="inputEmail" class="form-control mb-3" placeholder="Email address" required="" autofocus="">
     <div class="alert alert-danger" role="alert" id="emailError"></div>
@@ -9,5 +9,6 @@
     <div class="alert alert-danger" role="alert" id="passwordError"></div>
 
     <div id="spinner"></div>
-    <button class="btn btn-lg btn-primary btn-block w-100" type="submit" id="signInBtn">Sign in</button>
+    <button class="btn btn-lg btn-primary btn-block w-100" type="submit" id="signInBtn">Zaloguje się</button>
+    <h6 class="mt-2">Jeżeli nie posiadasz konta <a href="/rejestracja">Kliknij tutaj</a> aby się zarejestrować!</h6>
 </form>
